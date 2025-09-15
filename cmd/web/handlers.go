6334a0e8b7438f634e9cddfa3ac33b6b2ed9666a
Ali Asaad <ali.asaad@miswag.com>
2025-09-15 21:32:42 +0300

    add: injectable error handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -35,7 +35,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -45,7 +45,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Snippet ID: %d", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r * http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r * http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow","POST")
 		http.Error(w, "method not allowd!", http.StatusMethodNotAllowed)
